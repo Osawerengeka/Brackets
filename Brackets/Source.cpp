c7339f7c6263a1db8bdf45779ad83a07e965951c
Osawerengeka <mr.georgegevork@gmail.com>
2021-03-09 21:29:18 +0300

    fixing errors

--- Brackets/Source.cpp
+++ Brackets/Source.cpp
@@ -1,59 +1,53 @@
-#include <iostream>
+#include <fstream>
 #include <string>
-#include <vector>
+#include <iostream>
+using namespace std;
 
 
 void Stack()
 {
-
-	std::string pro;
-	while(std::cin >> pro)
+	string pro;
+	while(cin >> pro)
 	{
 		int flag = 0, top = 0;
 		int depth = 0;
 		int l = pro.size();
-		std::vector <char> arr(l);
+		char *arr = new char[l];
 		for (int i = 0; i < l; i++)
 		{
-			if ((pro[i] == '[') || (pro[i] == '('))
+			if ((pro[i] == '[') || (pro[i] == '(') || (pro[i] == '{'))
 			{
 				depth++;
+				arr[top++] = pro[i];
 			}
 			else
 			{
 				depth--;
 			}
 
-			if ((pro[i] == '(') || (pro[i] == '['))
-			{
-				arr[top++] = pro[i];
-			}
-
-			if ((pro[i] == ')') && (arr[top - 1] == '(') && (top >= 0))
+			if (((pro[i] == ')') && (arr[top - 1] == '(') ||
+				 (pro[i] == ']') && (arr[top - 1] == '[') || 
+				 (pro[i] == '}') && (arr[top - 1] == '{'))&& 
+				 (top >= 0))
 			{
 				top--;
 			}
-			else if ((pro[i] == ')') && (arr[top - 1] != '('))
+			else if (((pro[i] == ')') && (arr[top - 1] != '(')) ||
+					((pro[i] == ']') && (arr[top - 1] != '[')) ||
+					((pro[i] == '}') && (arr[top - 1] != '{')))
 			{
 				flag++;
 			}
 
-			if ((pro[i] == ']') && (arr[top - 1] == '[') && (top >= 0))
-			{
-				top--;
-			}
-			else if ((pro[i] == ']') && (arr[top - 1] != '['))
-			{
-				flag++;
-			}
+			
 		}
 		if ((flag == 0) && (depth == 0))
 		{
-			std::cout << "TRUE " << std::endl;
+			cout << "TRUE " << endl;
 		}
 		else
 		{
-			std::cout << "FALSE " << std::endl;
+			cout << "FALSE " << endl;
 		}
 	}
 }
